164089ce8096ec2efafab2f32c404cc5fd6fc275
AlanaZUP <alana.reis@zup.com.br>
2021-09-16 11:28:56 -0300

    Adicionando filtro com base no documento

--- src/main/java/com/zupacademy/propostas/proposta/PropostaRepository.java
+++ src/main/java/com/zupacademy/propostas/proposta/PropostaRepository.java
@@ -2,5 +2,9 @@ package com.zupacademy.propostas.proposta;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PropostaRepository extends JpaRepository<Proposta, Long> {
+
+    List<Proposta> findByDocumento(String documento);
 }
