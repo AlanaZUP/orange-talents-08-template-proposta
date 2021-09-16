99239d5b555ee65909ce53a98dd4d4a4c7bf5760
AlanaZUP <alana.reis@zup.com.br>
2021-09-16 11:28:09 -0300

    toModel recebe repository

--- src/main/java/com/zupacademy/propostas/proposta/cadastra/PropostaRequest.java
+++ src/main/java/com/zupacademy/propostas/proposta/cadastra/PropostaRequest.java
@@ -1,13 +1,16 @@
 package com.zupacademy.propostas.proposta.cadastra;
 
+import com.zupacademy.propostas.commos.exceptions.ValorDuplicadoException;
 import com.zupacademy.propostas.commos.validations.document.CpfOrCnpj;
 import com.zupacademy.propostas.proposta.Proposta;
+import com.zupacademy.propostas.proposta.PropostaRepository;
+import org.springframework.util.Assert;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
-import java.math.BigDecimal;
+import java.util.List;
 
 public class PropostaRequest {
 
@@ -30,7 +33,14 @@ public class PropostaRequest {
         this.salario = salario;
     }
 
-    public Proposta toModel(){
+    public Proposta toModel(PropostaRepository propostaRepository){
+        List<Proposta> propostas = propostaRepository.findByDocumento(this.documento);
+
+        if (propostas.size() == 1){
+            throw new ValorDuplicadoException("documento", "Já existe um documento com esse valor.");
+        }
+        Assert.isTrue(propostas.size() == 0, "Bug geral, existe mais de uma proposta com esse documento :o !!");
+
         return new Proposta(documento, nome, email, endereco, salario);
     }
 }
