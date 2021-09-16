75b4c22002a9d494f4e5ffb0ae9211beffa8b5ed
AlanaZUP <alana.reis@zup.com.br>
2021-09-16 10:21:14 -0300

    Repository proposta

--- src/main/java/com/zupacademy/propostas/proposta/PropostaRepository.java
+++ src/main/java/com/zupacademy/propostas/proposta/PropostaRepository.java
@@ -1,2 +1,6 @@
-package com.zupacademy.propostas.proposta;public interface PropostaRepository {
+package com.zupacademy.propostas.proposta;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PropostaRepository extends JpaRepository<Proposta, Long> {
 }
