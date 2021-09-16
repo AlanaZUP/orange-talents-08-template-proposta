1564ed7e2ac4490a099213fed1dc83c3a8bb4f7f
AlanaZUP <alana.reis@zup.com.br>
2021-09-16 10:20:53 -0300

    Entity proposta

--- src/main/java/com/zupacademy/propostas/proposta/Proposta.java
+++ src/main/java/com/zupacademy/propostas/proposta/Proposta.java
@@ -1,2 +1,61 @@
-package com.zupacademy.propostas.proposta;public class Proposta {
+package com.zupacademy.propostas.proposta;
+
+import com.zupacademy.propostas.commos.validations.document.CpfOrCnpj;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+@Entity
+public class Proposta {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank @CpfOrCnpj
+    private String documento;
+    @NotBlank
+    private String nome;
+    @NotBlank @Email
+    private String email;
+    @NotBlank
+    private String endereco;
+    @NotNull @Positive
+    private Double salario;
+
+    public Proposta(@NotBlank String documento, @NotBlank String nome, @NotBlank @Email String email, @NotBlank String endereco, @NotNull @Positive Double salario) {
+        this.documento = documento;
+        this.nome = nome;
+        this.email = email;
+        this.endereco = endereco;
+        this.salario = salario;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public Double getSalario() {
+        return salario;
+    }
 }
