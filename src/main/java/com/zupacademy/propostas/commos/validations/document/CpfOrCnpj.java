d78bbb809f11c777d89b257f70fb680eaa1004c4
AlanaZUP <alana.reis@zup.com.br>
2021-09-16 10:22:07 -0300

    Validação personalidade para aceitar CPF ou CNPJ

--- src/main/java/com/zupacademy/propostas/commos/validations/document/CpfOrCnpj.java
+++ src/main/java/com/zupacademy/propostas/commos/validations/document/CpfOrCnpj.java
@@ -1,2 +1,27 @@
-package com.zupacademy.propostas.commos.validations.document;public @interface CpfOrCnpj {
+package com.zupacademy.propostas.commos.validations.document;
+
+import org.hibernate.validator.constraints.CompositionType;
+import org.hibernate.validator.constraints.ConstraintComposition;
+import org.hibernate.validator.constraints.br.CNPJ;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import javax.validation.ReportAsSingleViolation;
+import java.lang.annotation.*;
+
+@CPF
+@CNPJ
+@ConstraintComposition(CompositionType.OR) // specifies OR as boolean operator instead of AND
+@ReportAsSingleViolation // the error reports of each individual composing constraint are ignored
+@Constraint(validatedBy = { }) // we don't need a validator :-)
+@Documented
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CpfOrCnpj {
+    String message() default "Não é um CPF ou CNPJ válido";
+
+    Class<?>[] groups() default { };
+
+    Class<? extends Payload>[] payload() default { };
 }
