e5ecc94fc497c86ed803abc8ce1af90ee1ba9b4e
AlanaZUP <alana.reis@zup.com.br>
2021-09-16 10:50:17 -0300

    Adicionando log ao cadastrar proposta

---
+++ src/main/java/com/zupacademy/propostas/commos/logs/LogExample.java
@@ -0,0 +1,16 @@
+package com.zupacademy.propostas.commos.logs;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LogExample {
+    private final Logger logger = LoggerFactory.getLogger(LogExample.class);
+
+    public void log(){
+        logger.info("Log de informação");
+        logger.warn("Log de aviso, algo está errado ou faltando! cuidado!");
+        logger.error("Log de erro, algo de errado aconteceu!");
+        logger.debug("Log de depuração, contém informações mais refinadas, que são mais úteis para depurar um aplicativo");
+        logger.trace("Log de rastreabilidade, contém informações mais refinadas do que o DEBUG");
+    }
+}
--- src/main/java/com/zupacademy/propostas/proposta/cadastra/CadastraProposta.java
+++ src/main/java/com/zupacademy/propostas/proposta/cadastra/CadastraProposta.java
@@ -1,7 +1,10 @@
 package com.zupacademy.propostas.proposta.cadastra;
 
+import com.zupacademy.propostas.commos.logs.LogExample;
 import com.zupacademy.propostas.proposta.Proposta;
 import com.zupacademy.propostas.proposta.PropostaRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,12 +21,16 @@ public class CadastraProposta {
     @Autowired
     private PropostaRepository propostaRepository;
 
+    private final Logger logger = LoggerFactory.getLogger(LogExample.class);
+
     @PostMapping @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<Object> cadastraProposta(@RequestBody @Valid PropostaRequest propostaRequest, UriComponentsBuilder uriBuilder){
         Proposta proposta = propostaRequest.toModel();
         propostaRepository.save(proposta);
 
         URI uri = uriBuilder.path("/propostas/{id}").build(proposta.getId());
+
+        logger.info("Proposta documento={} salário={} criada com sucesso!", proposta.getDocumento(), proposta.getSalario());
         return ResponseEntity.created(uri).build();
     }
 }
