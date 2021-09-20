d65d101c8df3c2b307134af04f0141e475558b5d
AlanaZUP <alana.reis@zup.com.br>
2021-09-20 14:43:22 -0300

    Chamadno analise da proposta

--- src/main/java/com/zupacademy/propostas/proposta/cadastra/CadastraProposta.java
+++ src/main/java/com/zupacademy/propostas/proposta/cadastra/CadastraProposta.java
@@ -1,8 +1,10 @@
 package com.zupacademy.propostas.proposta.cadastra;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.zupacademy.propostas.commos.logs.LogExample;
 import com.zupacademy.propostas.proposta.Proposta;
 import com.zupacademy.propostas.proposta.PropostaRepository;
+import com.zupacademy.propostas.proposta.cadastra.analisaSolicitacao.AnalisaSolicitacao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,12 +23,18 @@ public class CadastraProposta {
     @Autowired
     private PropostaRepository propostaRepository;
 
+    @Autowired
+    private AnalisaSolicitacao analisaSolicitacao;
+
     private final Logger logger = LoggerFactory.getLogger(LogExample.class);
 
     @PostMapping @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<Object> cadastraProposta(@RequestBody @Valid PropostaRequest propostaRequest, UriComponentsBuilder uriBuilder){
+    public ResponseEntity<Object> cadastraProposta(@RequestBody @Valid PropostaRequest propostaRequest, UriComponentsBuilder uriBuilder) throws JsonProcessingException {
         Proposta proposta = propostaRequest.toModel(propostaRepository);
         propostaRepository.save(proposta);
+
+        proposta.analisaSolicitacao(analisaSolicitacao);
+        proposta = propostaRepository.save(proposta);
 
         URI uri = uriBuilder.path("/propostas/{id}").build(proposta.getId());
 
