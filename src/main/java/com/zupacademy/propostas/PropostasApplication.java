8bbbfb5e6c89cd7cba2ad523266ce5df5093f87e
AlanaZUP <alana.reis@zup.com.br>
2021-09-20 14:39:43 -0300

    Habilitando Feign

--- src/main/java/com/zupacademy/propostas/PropostasApplication.java
+++ src/main/java/com/zupacademy/propostas/PropostasApplication.java
@@ -2,9 +2,10 @@ package com.zupacademy.propostas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
-
+@EnableFeignClients
 @SpringBootApplication
 public class PropostasApplication {
 
