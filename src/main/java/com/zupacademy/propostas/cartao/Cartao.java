9dead3e1aefeb475d218a7180c209ffdf8d5e1e6
AlanaZUP <alana.reis@zup.com.br>
2021-09-21 08:36:50 -0300

    Entidade que guarda os dados do cartão

--- src/main/java/com/zupacademy/propostas/cartao/Cartao.java
+++ src/main/java/com/zupacademy/propostas/cartao/Cartao.java
@@ -1,7 +1,42 @@
 package com.zupacademy.propostas.cartao;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Entity
 public class Cartao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false, unique = true)
+    private String numeroCartao;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String titular;
+
+    @NotNull
+    @Column(nullable = false)
+    private LocalDateTime emitidoEm;
+
+    @NotNull
+    @Column(nullable = false)
+    private BigDecimal limite;
+
+    @Deprecated
+    public Cartao() {
+    }
+
+    public Cartao(String numeroCartao, String titular, LocalDateTime emitidoEm, BigDecimal limite) {
+        this.numeroCartao = numeroCartao;
+        this.titular = titular;
+        this.emitidoEm = emitidoEm;
+        this.limite = limite;
+    }
 }
