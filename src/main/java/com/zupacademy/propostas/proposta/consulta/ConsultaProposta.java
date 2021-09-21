e452bddf0ae4eae6eff961206c8caa0279c1ef0c
AlanaZUP <alana.reis@zup.com.br>
2021-09-21 09:27:54 -0300

    Controller que recebe requisição GET da consulta

--- src/main/java/com/zupacademy/propostas/proposta/consulta/ConsultaProposta.java
+++ src/main/java/com/zupacademy/propostas/proposta/consulta/ConsultaProposta.java
@@ -1,2 +1,23 @@
-package com.zupacademy.propostas.proposta.consulta;public class ConsultaProposta {
+package com.zupacademy.propostas.proposta.consulta;
+
+import com.zupacademy.propostas.proposta.Proposta;
+import com.zupacademy.propostas.proposta.PropostaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping("/propostas")
+public class ConsultaProposta {
+
+    @Autowired
+    private PropostaRepository propostaRepository;
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public PropostaConsultaDTO consulta(@PathVariable("id") Long idProposta){
+        Proposta proposta = propostaRepository.findById(idProposta).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Não existe uma proposta com a identificação informada"));
+        return new PropostaConsultaDTO(proposta);
+    }
 }
