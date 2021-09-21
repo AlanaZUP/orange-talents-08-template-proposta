94e2e7009cfdc41cc823bb11ca3cbed1660266b4
AlanaZUP <alana.reis@zup.com.br>
2021-09-21 09:27:25 -0300

    DTO da proposta para saida da consulta

--- src/main/java/com/zupacademy/propostas/proposta/consulta/PropostaConsultaDTO.java
+++ src/main/java/com/zupacademy/propostas/proposta/consulta/PropostaConsultaDTO.java
@@ -1,2 +1,67 @@
-package com.zupacademy.propostas.proposta.consulta;public class PropostaConsultaDTO {
+package com.zupacademy.propostas.proposta.consulta;
+
+import com.zupacademy.propostas.cartao.Cartao;
+import com.zupacademy.propostas.commos.validations.document.CpfOrCnpj;
+import com.zupacademy.propostas.proposta.Proposta;
+import com.zupacademy.propostas.proposta.StatusProposta;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+public class PropostaConsultaDTO {
+
+    private Long id;
+    private String documento;
+    private String nome;
+    private String email;
+    private String endereco;
+    private Double salario;
+    private Cartao cartao;
+    private StatusProposta statusProposta;
+
+    public PropostaConsultaDTO(Proposta proposta) {
+        this.id = proposta.getId();
+        this.documento = proposta.getDocumento();
+        this.nome = proposta.getNome();
+        this.email = proposta.getEmail();
+        this.endereco = proposta.getEndereco();
+        this.salario = proposta.getSalario();
+        this.cartao = proposta.getCartao();
+        this.statusProposta = proposta.getStatusProposta();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public Double getSalario() {
+        return salario;
+    }
+
+    public Cartao getCartao() {
+        return cartao;
+    }
+
+    public StatusProposta getStatusProposta() {
+        return statusProposta;
+    }
 }
