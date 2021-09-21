818162e2175a78403220e44c42b1f553df0f2b36
AlanaZUP <alana.reis@zup.com.br>
2021-09-21 08:37:43 -0300

    Classe que faz a requisição periódica para solicitar cartões as propostas elegiveis

--- src/main/java/com/zupacademy/propostas/cartao/ObservaCartao.java
+++ src/main/java/com/zupacademy/propostas/cartao/ObservaCartao.java
@@ -1,13 +1,40 @@
-package com.zupacademy.propostas.teste;
+package com.zupacademy.propostas.cartao;
 
+import com.zupacademy.propostas.proposta.Proposta;
+import com.zupacademy.propostas.proposta.PropostaRepository;
+import com.zupacademy.propostas.proposta.StatusProposta;
+import feign.FeignException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-@Component
-public class Test {
+import java.util.List;
 
-    @Scheduled(fixedDelay = 5000)
-    public void teste(){
-        System.out.println("Estou sendo mostrado de 5 em 5 segundos");
+@Component
+public class ObservaCartao {
+
+    @Autowired
+    private PropostaRepository propostaRepository;
+
+    @Autowired
+    private SolicitaCartao solicitaCartao;
+
+    @Scheduled(fixedDelayString = "${periodicidade.observa-cartao}")
+    public void observa(){
+        List<Proposta> propostasAll = propostaRepository.findAll();
+        List<Proposta> propostas = propostaRepository.findBystatusPropostaAndCartaoIsNull(StatusProposta.ELEGIVEL);
+        propostas.forEach(proposta -> {
+            SolicitaCartaoRequest cartaoRequest = new SolicitaCartaoRequest(proposta.getDocumento(), proposta.getNome(), proposta.getId());
+            try{
+                SolicitaCartaoResponse cartaoResponse = solicitaCartao.solicita(cartaoRequest);
+                Cartao cartao = cartaoResponse.toModel();
+                proposta.adicionaCartao(cartao);
+                propostaRepository.save(proposta);
+                System.out.println("Consegui um cartao");
+            }
+            catch (FeignException feignException){
+                System.out.println("Nao consegui um cartao");
+            }
+        });
     }
 }
