99998870e91576ab77dd93dce81b2084abf496fa
AlanaZUP <alana.reis@zup.com.br>
2021-09-21 08:35:33 -0300

    Feign que realiza a requisição do cartão

--- src/main/java/com/zupacademy/propostas/cartao/SolicitaCartao.java
+++ src/main/java/com/zupacademy/propostas/cartao/SolicitaCartao.java
@@ -1,2 +1,14 @@
-package com.zupacademy.propostas.cartao;public class SolicitaCartao {
+package com.zupacademy.propostas.cartao;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.validation.Valid;
+
+@FeignClient(name = "cartao", url = "${feign.url.cartao}")
+public interface SolicitaCartao {
+
+    @PostMapping
+    public SolicitaCartaoResponse solicita(@RequestBody @Valid SolicitaCartaoRequest cartaoRequest);
 }
