9ad05e157d8ccbc7859140967be63cf5dab73750
AlanaZUP <alana.reis@zup.com.br>
2021-09-21 08:35:59 -0300

    DTO que envia os dados necessários para a requisição do cartão

--- src/main/java/com/zupacademy/propostas/cartao/SolicitaCartaoRequest.java
+++ src/main/java/com/zupacademy/propostas/cartao/SolicitaCartaoRequest.java
@@ -1,2 +1,36 @@
-package com.zupacademy.propostas.cartao;public class SolicitaCartaoRequest {
+package com.zupacademy.propostas.cartao;
+
+import com.zupacademy.propostas.commos.validations.document.CpfOrCnpj;
+import com.zupacademy.propostas.commos.validations.existsId.ExistisId;
+import com.zupacademy.propostas.proposta.Proposta;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+public class SolicitaCartaoRequest {
+
+    @CpfOrCnpj @NotBlank
+    private String documento;
+    @NotBlank
+    private String nome;
+    @NotNull @ExistisId(classe = Proposta.class, acceptedNull = false)
+    private Long idProposta;
+
+    public SolicitaCartaoRequest(String documento, String nome, Long idProposta) {
+        this.documento = documento;
+        this.nome = nome;
+        this.idProposta = idProposta;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Long getIdProposta() {
+        return idProposta;
+    }
 }
