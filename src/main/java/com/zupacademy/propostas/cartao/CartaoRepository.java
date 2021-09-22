08a17423275a99c461058b7d5ed822b08903d6bc
AlanaZUP <alana.reis@zup.com.br>
2021-09-22 13:52:14 -0300

    Repositorio do cartão

--- src/main/java/com/zupacademy/propostas/cartao/CartaoRepository.java
+++ src/main/java/com/zupacademy/propostas/cartao/CartaoRepository.java
@@ -1,2 +1,6 @@
-package com.zupacademy.propostas.cartao;public class CartaoRepository {
+package com.zupacademy.propostas.cartao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartaoRepository extends JpaRepository<Cartao, Long> {
 }
