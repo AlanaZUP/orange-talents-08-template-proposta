492bbb41027cb7bbf31ed110bb314370624becd4
AlanaZUP <alana.reis@zup.com.br>
2021-09-22 13:51:22 -0300

    Controller que recebe a requisição de cadastrar biometria

--- src/main/java/com/zupacademy/propostas/cartao/biometria/CadastraBiometria.java
+++ src/main/java/com/zupacademy/propostas/cartao/biometria/CadastraBiometria.java
@@ -1,2 +1,36 @@
-package com.zupacademy.propostas.cartao.biometria;public class CadastraBiometria {
+package com.zupacademy.propostas.cartao.biometria;
+
+import com.zupacademy.propostas.cartao.Cartao;
+import com.zupacademy.propostas.cartao.CartaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+import java.net.URI;
+
+@RestController
+@RequestMapping("/cartoes")
+public class CadastraBiometria {
+
+    @Autowired
+    private CartaoRepository cartaoRepository;
+
+    @PostMapping("/{id}/biometrias")
+    @ResponseStatus(HttpStatus.CREATED)
+    @Transactional
+    public ResponseEntity cadastraBiometria(@PathVariable("id") Long idCartao, @RequestBody @Valid BiometriaRequest biometriaRequest, UriComponentsBuilder uriBuilder){
+        Cartao cartao = cartaoRepository.findById(idCartao).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Não foi encontrado um cartão com o id informado"));
+        Biometria biometria = biometriaRequest.toModel(cartao);
+        cartao.adicionaBiomatria(biometria);
+
+        cartaoRepository.save(cartao);
+
+        URI uri = uriBuilder.path("/cartoes/{idCartao}/biometrias/{idBiomatrie}").build(cartao.getId(), biometria.getUuid());
+        return ResponseEntity.created(uri).build();
+    }
 }
