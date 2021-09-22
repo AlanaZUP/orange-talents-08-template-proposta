420e4dad96eaa11cb36b80c5db1ad9633134926e
AlanaZUP <alana.reis@zup.com.br>
2021-09-22 13:50:08 -0300

    DTO que recebe os dados da biometria

--- src/main/java/com/zupacademy/propostas/cartao/biometria/BiometriaRequest.java
+++ src/main/java/com/zupacademy/propostas/cartao/biometria/BiometriaRequest.java
@@ -1,2 +1,20 @@
-package com.zupacademy.propostas.cartao.biometria;public class BiometriaRequest {
+package com.zupacademy.propostas.cartao.biometria;
+
+import com.zupacademy.propostas.cartao.Cartao;
+import com.zupacademy.propostas.commos.validations.isBase64.IsBase64;
+
+import javax.validation.constraints.NotBlank;
+
+public class BiometriaRequest {
+
+    @NotBlank @IsBase64
+    private String fingerprint;
+
+    public void setFingerprint(String fingerprint) {
+        this.fingerprint = fingerprint;
+    }
+
+    public Biometria toModel(Cartao cartao) {
+        return new Biometria(this.fingerprint, cartao);
+    }
 }
