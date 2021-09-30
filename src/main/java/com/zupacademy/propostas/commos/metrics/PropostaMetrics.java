b126971d76a2ad4667813fc9c627080cdbea7618
AlanaZUP <alana.reis@zup.com.br>
2021-09-30 09:41:24 -0300

    Métrica que calcula quantidade de propostas

--- src/main/java/com/zupacademy/propostas/commos/metrics/PropostaMetrics.java
+++ src/main/java/com/zupacademy/propostas/commos/metrics/PropostaMetrics.java
@@ -1,2 +1,28 @@
-package com.zupacademy.propostas.commos.metrics;public class PropostaMetrics {
+package com.zupacademy.propostas.commos.metrics;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tag;
+import io.micrometer.core.instrument.Timer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Component
+public class PropostaMetrics {
+
+    @Autowired
+    private MeterRegistry meterRegistry;
+
+    public void contadorPropostas(){
+        Collection<Tag> tags = new ArrayList<>();
+        tags.add(Tag.of("emissora", "Mastercard"));
+        tags.add(Tag.of("banco", "Itaú"));
+
+        Counter contadorDePropostasCriadas = this.meterRegistry.counter("proposta_criada", tags);
+        contadorDePropostasCriadas.increment();
+    }
+
 }
