6a2a7a4129fe255c242547c99bb881d608dbc9bd
AlanaZUP <alana.reis@zup.com.br>
2021-09-30 11:07:53 -0300

    DTO retorno da notificação da viagem

--- src/main/java/com/zupacademy/propostas/cartao/viagem/NotificaViagemResponse.java
+++ src/main/java/com/zupacademy/propostas/cartao/viagem/NotificaViagemResponse.java
@@ -1,2 +1,21 @@
-package com.zupacademy.propostas.cartao.viagem;public class NotificaViagemResponse {
+package com.zupacademy.propostas.cartao.viagem;
+
+import javax.validation.constraints.NotNull;
+
+public class NotificaViagemResponse {
+
+    @NotNull
+    private StatusNotificaViagem resultado;
+
+    @Deprecated
+    public NotificaViagemResponse() {
+    }
+
+    public NotificaViagemResponse(StatusNotificaViagem resultado) {
+        this.resultado = resultado;
+    }
+
+    public StatusNotificaViagem getResultado() {
+        return resultado;
+    }
 }
