4866088784981c7ebadbae1bf2bdc2cb56aa1c82
AlanaZUP <alana.reis@zup.com.br>
2021-09-30 11:07:25 -0300

    DTO request para notificar a viagem

--- src/main/java/com/zupacademy/propostas/cartao/viagem/NotificaViagemRequest.java
+++ src/main/java/com/zupacademy/propostas/cartao/viagem/NotificaViagemRequest.java
@@ -1,2 +1,32 @@
-package com.zupacademy.propostas.cartao.viagem;public class NotificaViagemRequest {
+package com.zupacademy.propostas.cartao.viagem;
+
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+public class NotificaViagemRequest {
+
+    @NotBlank
+    private String destino;
+
+    @NotNull @FutureOrPresent
+    private LocalDate validoAte;
+
+    @Deprecated
+    public NotificaViagemRequest() {
+    }
+
+    public NotificaViagemRequest(String destino, LocalDate validoAte) {
+        this.destino = destino;
+        this.validoAte = validoAte;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public LocalDate getValidoAte() {
+        return validoAte;
+    }
 }
