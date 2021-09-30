6777d98b85490b5604966fdec54352f6f571e143
AlanaZUP <alana.reis@zup.com.br>
2021-09-30 10:38:14 -0300

    DTO Request Viagem

--- src/main/java/com/zupacademy/propostas/cartao/viagem/ViagemRequest.java
+++ src/main/java/com/zupacademy/propostas/cartao/viagem/ViagemRequest.java
@@ -1,2 +1,26 @@
-package com.zupacademy.propostas.cartao.viagem;public class ViagemRequest {
+package com.zupacademy.propostas.cartao.viagem;
+
+import com.zupacademy.propostas.cartao.Cartao;
+
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+public class ViagemRequest {
+
+    @NotBlank
+    private String destinoViagem;
+
+    @NotNull @FutureOrPresent
+    private LocalDate dataTermino;
+
+    public ViagemRequest(String destinoViagem, LocalDate dataTermino) {
+        this.destinoViagem = destinoViagem;
+        this.dataTermino = dataTermino;
+    }
+
+    public Viagem toModel(String ipRequisicao, String userAgenteRequisicao, Cartao cartao){
+        return new Viagem(destinoViagem, ipRequisicao, userAgenteRequisicao, dataTermino, cartao);
+    }
 }
