e5000ab061aa59b910189abd57eb94dbb57db60c
AlanaZUP <alana.reis@zup.com.br>
2021-10-23 19:23:15 -0300

    Handle para exceptions NotFound e RegraDeNegocio, handle para exceptions em gerais e loggs de erros

--- src/main/java/com/zupacademy/propostas/commos/exceptions/ValidationErroHandler.java
+++ src/main/java/com/zupacademy/propostas/commos/exceptions/ValidationErroHandler.java
@@ -1,6 +1,8 @@
 package com.zupacademy.propostas.commos.exceptions;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,28 +24,88 @@ public class ValidationErroHandler {
     @Autowired
     private MessageSource messageSource;
 
+    private Logger LOGGER = LoggerFactory.getLogger(ValidationErroHandler.class);
+
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public List<ErroResponseDTO> handle(MethodArgumentNotValidException exception){
-        List<ErroResponseDTO> erros = new ArrayList<>();
+    public StandardError handle(MethodArgumentNotValidException exception){
+        List<ErroResponse> erros = new ArrayList<>();
         List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
         fieldErrors.forEach(e -> {
             String message = messageSource.getMessage(e, LocaleContextHolder.getLocale());
-            ErroResponseDTO erro = new ErroResponseDTO(e.getField(), message);
+            ErroResponse erro = new ErroResponse(e.getField(), e.getRejectedValue(), message);
             erros.add(erro);
         });
 
-        return erros;
+        LOGGER.warn("ERROR BAD REQUEST -> \n" + erros.stream().map(it -> it.toString()+"\n").toString());
+
+        return new StandardError(
+                LocalDateTime.now(),
+                "/erros/validations",
+                "Falha ao validar dados",
+                HttpStatus.BAD_REQUEST,
+                "Os dados recebidos não estão no formato adequado, tente novamente",
+                erros
+        );
     }
 
 
-    @ExceptionHandler(ApiErroException.class)
-    public ResponseEntity<List<ErroResponseDTO>> handleValorDuplicado(ApiErroException exception){
-        List<ErroResponseDTO> erros = new ArrayList<>();
-        ErroResponseDTO erro = new ErroResponseDTO(exception.getCampo(), exception.getMessage());
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    @ExceptionHandler(RegraDeNegocioException.class)
+    public StandardError handleRegraDeNegocio(RegraDeNegocioException exception){
+        List<ErroResponse> erros = new ArrayList<>();
+        ErroResponse erro = new ErroResponse(exception.getField(), exception.getValue(), exception.getMessage());
 
         erros.add(erro);
 
-        return ResponseEntity.status(exception.getHttpStatus()).body(erros);
+        LOGGER.warn("ERROR UNPROCESSABLE_ENTITY -> \n" + erros.stream().map(it -> it.toString()+"\n").toString());
+
+        return new StandardError(
+                LocalDateTime.now(),
+                "/errors/regra-de-negocio",
+                "Não atende ás regras da aplicação",
+                HttpStatus.UNPROCESSABLE_ENTITY,
+                "Os dados recebidos não estão de acordo com as regras de negócio da aplicação, tente novamente",
+                erros
+        );
+    }
+
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(RegraDeNegocioException.class)
+    public StandardError handleNotFound(NotFoundException exception){
+        List<ErroResponseNotFound> erros = new ArrayList<>();
+        ErroResponseNotFound erro = new ErroResponseNotFound(exception.getEntity(), exception.getField(), exception.getValue(), exception.getMessage());
+
+        erros.add(erro);
+
+        LOGGER.warn("ERROR NOT FOUND -> \n" + erros.stream().map(it -> it.toString()+"\n").toString());
+
+        return new StandardError(
+                LocalDateTime.now(),
+                "/errors/regra-de-negocio",
+                "Não atende ás regras da aplicação",
+                HttpStatus.UNPROCESSABLE_ENTITY,
+                "Os dados recebidos não estão de acordo com as regras de negócio da aplicação, tente novamente",
+                erros
+        );
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public StandardError handleException(Exception ex) {
+
+        List<Exception> erros = new ArrayList<>();
+        erros.add(ex);
+        LOGGER.error("ERROR INTERNAL -> \n" + erros.stream().map(it -> it.toString()+"\n").toString());
+
+        return new StandardError(
+                LocalDateTime.now(),
+                "/erros/internal-error",
+                "Ocorreu um erro no servidor",
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                "Não foi possível executar a requisição devido a um erro interno no servidor, tente novamente mais tarde",
+                erros
+        );
     }
 }
