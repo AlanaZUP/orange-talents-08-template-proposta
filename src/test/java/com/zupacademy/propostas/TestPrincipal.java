0dfc124289ad409972e3daaee743e348388c03b1
AlanaZUP <alana.reis@zup.com.br>
2021-10-24 19:07:34 -0300

    Configurações comuns para fazer os testes

--- src/test/java/com/zupacademy/propostas/TestPrincipal.java
+++ src/test/java/com/zupacademy/propostas/TestPrincipal.java
@@ -1,2 +1,19 @@
-package com.zupacademy.propostas;public class TestPrincipal {
+package com.zupacademy.propostas;
+
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class TestPrincipal {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    protected Gson gson = new Gson();
 }
